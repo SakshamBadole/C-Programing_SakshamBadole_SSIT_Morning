485e0ccfb157982f97b8d6d8e447887b031fbeea
SakshamBadole <sakshambadole19@gmail.com>
2025-01-16 00:05:45 +0530

    Add files via upload

--- Array Que.c
+++ Array Que.c
@@ -7,7 +7,7 @@
 
 
 
-//Que no.2
+//Que no.2 all zeros on left
 #include <stdio.h>
 void main(){
 	int i,a[5]={1,0,8,0,5};
@@ -32,7 +32,7 @@ for(k=0; k<5; k++){
 }
 
 
-//Que no.3
+//Que no.3 even odd separate
 #include <stdio.h>
 void main(){
 	int i,a[5]={5,2,1,4,3};
@@ -57,14 +57,14 @@ for(k=0; k<5; k++){
 }
 
 
-// Que no.4
+// Que no.4 accending order
 #include <stdio.h>
 void main(){
-	int i,a[3]={3,1,2}, temp;
+	int i,j,a[6]={3,1,2,5,8,4},temp;
 	printf("Enter array elements:");
 	 
-	for(i=0; i<3; i++){
-	for(j=i+1; j<3; j++)
+	for(i=0; i<6; i++){
+	for(j=i+1; j<6; j++)
 		if(a[i]>a[j]){
 			temp=a[i];
 			a[i]=a[j];
@@ -72,12 +72,12 @@ void main(){
 			
 		}
 }
-for(i=0; i<3; i++){
+for(i=0; i<6; i++){
 	printf("%d",a[i]);
 }
 }
 	 
-// Que no.6
+// Que no.6 reverse order
 #include <stdio.h>
 void main(){
 	int i,a[5]={1,4,6,8,7,5};
@@ -88,7 +88,7 @@ void main(){
 }
 }
 	 
-//Que no.7
+//Que no.7 sum of all array
 #include <stdio.h>
 void main(){
 	int i,a[5]={1,4,6,8,7},sum=0;
@@ -100,7 +100,7 @@ printf("sum=%d",sum);
 }
 	 
 
-// Que no.9
+// Que no.9 maximum
 #include <stdio.h>
 void main(){
 	int i,a[3]={1,4,7}, max=a[0];
@@ -115,12 +115,12 @@ void main(){
 	printf("Maximum value is %d", max);
 }
 	 
-// Que no. 10
+// Que no. 10 minimum
 #include <stdio.h>
 void main(){
-	int i,a[3]={4,7,5,9}, min=a[0];
+	int i,a[4]={4,7,5,9}, min=a[0];
 
-	for(i=0; i<3; i++){
+	for(i=0; i<4; i++){
 		if(a[i]<min)
 		{
 			min=a[i];
